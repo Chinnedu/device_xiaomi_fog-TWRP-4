eefe9974e9136d5a2ea53023bf246ba8065378ca
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-08 14:57:37 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
-#
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
+This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -18,97 +17,76 @@
 # 	Please maintain this if you use this script or any part of it
 #
 FDEVICE="fog"
+
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
+    if [ -n "$chkdev" ]; then
+        FOX_BUILD_DEVICE="$FDEVICE"
+    else
+        chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+        [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+    fi
 }
 
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
+    fox_get_target_device
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+    export TARGET_ARCH=arm64
+    export OF_USE_MAGISKBOOT=1
+    export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+    export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+    export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+    export OF_NO_RELOAD_AFTER_DECRYPTION=1
+    export FOX_DISABLE_APP_MANAGER=1
+    export OF_MAINTAINER=masemoel
+    export FOX_VERSION=R11.1_2.3
+    export FOX_BUILD_TYPE="Stable"
+    export FOX_VARIANT=A12
+    export OF_SCREEN_H=2460
+    export OF_STATUS_H=122
+    export OF_STATUS_INDENT_LEFT=35
+    export OF_STATUS_INDENT_RIGHT=235
+    export OF_HIDE_NOTCH=1
+    export OF_ALLOW_DISABLE_NAVBAR=0
+    export OF_PATCH_AVB20=1
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
+    export OF_CHECK_OVERWRITE_ATTEMPTS=1
+    export OF_FBE_METADATA_MOUNT_IGNORE=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+    export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+    export OF_RUN_POST_FORMAT_PROCESS=1
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_ADVANCED_SECURITY=1
+    export FOX_NO_SAMSUNG_SPECIAL=1
+    export FOX_ENABLE_APP_MANAGER=1
+    export FOX_INSTALLER_DEBUG_MODE=1
+    export FOX_TARGET_DEVICES=fog
+    export OF_USE_GREEN_LED=0
+    export FOX_DELETE_AROMAFM=1
+    export OF_QUICK_BACKUP_LIST="/boot;/dtbo;/metadata;"
+    export OF_FORCE_PREBUILT_KERNEL=1
+    export FOX_REPLACE_BUSYBOX_PS=0
+    export FOX_USE_BASH_SHELL=1
+    export FOX_ASH_IS_BASH=1
+    export FOX_USE_NANO_EDITOR=1
+    export FOX_USE_TAR_BINARY=1
+    export FOX_USE_XZ_UTILS=1
+    export FOX_USE_SED_BINARY=1
+    export OF_ENABLE_LPTOOLS=1
+    export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
+    export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+    export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=1
+    export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
+    export OF_UNBIND_SDCARD_F2FS=1
 
- # Fox-specific flags
- export FOX_ENABLE_APP_MANAGER=0
- export FOX_DELETE_AROMAFM=1
- export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!
- export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
- export OF_QUICK_BACKUP_LIST="/boot;/data;/system;/metadata;"
- export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
- export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
- export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-
- # Binaries & Tools
- export FOX_USE_BASH_SHELL=1
- export FOX_ASH_IS_BASH=1
- export FOX_USE_NANO_EDITOR=1
- export FOX_USE_TAR_BINARY=1
- export FOX_USE_SED_BINARY=1
- export FOX_USE_XZ_UTILS=1
- export OF_ENABLE_LPTOOLS=1
-
- # Version & Variant
- export FOX_VERSION="R11.1"
- export FOX_VARIANT="A12.1"
-
- # MIUI & Custom ROMs
- export OF_VIRTUAL_AB_DEVICE=1
- export OF_NO_MIUI_PATCH_WARNING=1
- export OF_PATCH_AVB20=1
-
- # Disable Flashlight & Green LED
- export OF_FLASHLIGHT_ENABLE=0
- export OF_USE_GREEN_LED=0
-
- # Security (Disables MTP&ADB during password prompt)
- export FOX_ADVANCED_SECURITY=1
-
- # Screen settings
- export OF_SCREEN_H=2400
- export OF_STATUS_H=-70
- export OF_STATUS_INDENT_LEFT=48
- export OF_STATUS_INDENT_RIGHT=48
- export OF_ALLOW_DISABLE_NAVBAR=0
- export OF_HIDE_NOTCH=1
- export OF_CLOCK_POS=1 # Left & Right
-
- # Maximum permissible splash image size (in kilobytes); do *NOT* increase!
- export OF_SPLASH_MAX_SIZE=130
-
- # CCACHE
- export USE_CCACHE=1
- export CCACHE_EXEC=/usr/bin/ccache
-
- CCACHE_DIR="/media/${USERNAME}/ccache"
-
- if [ -d ${CCACHE_DIR} ];
-  then
-   export CCACHE_DIR=${CCACHE_DIR}
-  else
-   echo "CCACHE Directory/Partition is not mounted at \"${CCACHE_DIR}\""
-   echo "Please edit the CCACHE_DIR build variable or mount the directory."
- fi
-
- # Debugging
- ## export FOX_RESET_SETTINGS=0
- ## export FOX_INSTALLER_DEBUG_MODE=1
-
- # Other..
- export TW_DEFAULT_LANGUAGE="en"
- export LC_ALL="C"
-
- # Let's see what are our build VARs
- if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
- fi
+	# Let's see which are our build vars
+    if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+        export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+        export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+        export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    fi
 fi
-#
